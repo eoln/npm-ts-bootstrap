6aa094c811d5ccf6dc0a39d9a9ecffba82012a8c
eoln <2881004+eoln@users.noreply.github.com>
2018-11-12 20:04:28 +0100

    add test for pkg

--- index.spec.ts
+++ index.spec.ts
@@ -4,4 +4,8 @@ describe('index', () => {
   it('should be defined', () => {
     expect(index).toBeDefined();
   })
+
+  it('should export pkg', () => {
+    expect(index.pkg).toBeDefined();
+  })
 });
