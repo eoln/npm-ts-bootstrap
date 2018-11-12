d6e795532bd0b7d3799ac52d781c2350dbe9cd98
eoln <2881004+eoln@users.noreply.github.com>
2018-11-12 19:52:14 +0100

    first dist draft

---
+++ dist/index.spec.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index = require("./index");
+describe('index', () => {
+    it('should be defined', () => {
+        expect(index).toBeDefined();
+    });
+});
+//# sourceMappingURL=index.spec.js.map
